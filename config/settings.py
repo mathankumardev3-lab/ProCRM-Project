ebcd49a00b3da50a988c9fea8cdb7890e5afa34e
mathankumardev3-lab <mathankumar.dev3@gmail.com>
2026-09-18 12:16:04 +0530

    Add production error logging

--- config/settings.py
+++ config/settings.py
@@ -83,3 +83,19 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+    },
+}
